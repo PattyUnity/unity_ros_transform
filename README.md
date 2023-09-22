94408368e531a800c709df3139767a8b070cc347
PattyUnity <p222.yu@gmail.com>
2023-09-22 18:26:49 +0800

    odometry_fromodom_transformer.py is checked. occupancy_grid_transformer.py can run but output map cannot be echo. The output map topic name is on the list though.

--- README.md
+++ README.md
@@ -5,6 +5,14 @@
   ```sh
   rosbag play -l <rosbag.bag> /merged_desktop/tf:=/tf /merged_desktop/tf_static:=/tf_static
   ```
+  If tf_tree from bag file has time synchronizing issue, try using simulated time
+  ```sh
+  rosparam set use_sim_time true
+  ```
+  Then play bag file with --clock
+  ```sh
+  rosbag play --clock -l -s 1 -u 450 2023-06-14-16-58-28_for\ TCP\ test\ server\ for\ TCP\ test.bag /merged_desktop/tf:=/tf /merged_desktop/tf_static:=/tf_static
+  ```
 * Inspect tf tree
   ```sh
   rosrun rqt_tf_tree rqt_tf_tree
@@ -14,11 +22,32 @@
   roslaunch unity_ros_tf transformers.launch
   ```
 
+* git
+  Go to the package directory
+  ```sh
+  git status
+  git add .
+  git commit -m "Comment"
+  git push -f origin main
+  ```
+  
 
+
+### odometry_fromodom_transformer.py code flow
+Output = /global_odom = /original_odom + transform_child_to_parent_frame + z_offset_human + z_offset_floor
+1. transform_child_to_parent_frame = Lookup transform from tf tree between the parent_frame and child_frame
+    * parent_frame should be the upstream of the tf_tree, i.e. =  map
+    * child_frame should be the frame_id of the /original_odom. Look for it by ```rostopic echo /original_odom```
+2. z_offset_human = set as param in transformer.launch
+3. z_offset_floor = average of robot's odometry's z position (z position of transform_child_to_parent_frame, it is wrt global frame)
 
 ### odometry_transformer.py code flow
 Output = /global_odom = transform_child_to_parent_frame + z_offset_human + z_offset_floor
 1. transform_child_to_parent_frame = Lookup transform from tf tree between the parent_frame and child_frame
 2. z_offset_human = set as param in transformer.launch
-3. new_floor_reached = check the history of /is_climbing in bool_history[]. If bool_history = [[True]*consecutive_num + [False]*consecutive_num], means the robot has finished climbing. z_offset_floor is extracted.  
+<del> 3. new_floor_reached = check the history of /is_climbing in bool_history[]. If bool_history = [ [True]*consecutive_num + [False]*consecutive_num ], means the robot has finished climbing. z_offset_floor is extracted.  </del>
 4. z_offset_floor = average of robot's odometry's z position (z position of transform_child_to_parent_frame, it is wrt global frame)
+
+#### old odometry_transformer.py check for new_floor_reached event by listening to /is_climbing bool. 
+However, it hasn't been test yet. For now, we are going to use upper floor as Unity height = 0, and the lower floor will have -ve height that is hard-coded. 
+
--- src/occupancy_grid_transformer.py
+++ src/occupancy_grid_transformer.py
@@ -5,60 +5,110 @@ import tf2_ros
 from geometry_msgs.msg import TransformStamped, Transform
 from std_msgs.msg import Bool
 from nav_msgs.msg import Odometry, OccupancyGrid
-from tf.transformations import quaternion_from_euler
+import tf
 
 def map_callback(msg):
-    global original_map
+    global original_map, map_pub, tf_transform, updated_map, z_offset_floor, manual_position_offset, manual_orientation_offset,human_localization_pose,z_offset_human, is_localized
     original_map = msg
 
-def update_map_origin(floor_transform):
-    global updated_map
-    updated_map.info.origin.position.x += floor_transform.transform.translation.x
-    updated_map.info.origin.position.y += floor_transform.transform.translation.y
-    updated_map.info.origin.position.z += floor_transform.transform.translation.z
-    updated_map.info.origin.orientation.x += floor_transform.transform.rotation.x
-    updated_map.info.origin.orientation.y += floor_transform.transform.rotation.y
-    updated_map.info.origin.orientation.z += floor_transform.transform.rotation.z
-    updated_map.info.origin.orientation.w += floor_transform.transform.rotation.w
-
+    # Copy the original map
+    updated_map = original_map
     
+
+    # Add transform from the global frame (ros origin) to the original map origin
+    updated_map.info.origin.position.x += tf_transform.transform.translation.x
+    updated_map.info.origin.position.y += tf_transform.transform.translation.y
+    updated_map.info.origin.position.z += tf_transform.transform.translation.z
+    quaternion_combined = tf.transformations.quaternion_multiply(updated_map.info.origin.orientation, tf_transform.transform.rotation)
+    updated_map.info.origin.orientation.x = quaternion_combined[0]
+    updated_map.info.origin.orientation.y = quaternion_combined[1]
+    updated_map.info.origin.orientation.z = quaternion_combined[2]
+    updated_map.info.origin.orientation.w = quaternion_combined[3]
+
+    # Add floor transform (ususally only z height)
+    updated_map.info.origin.position.z += z_offset_floor
+
+    # Add manual transform
+    updated_map.info.origin.position.x += manual_position_offset[0]
+    updated_map.info.origin.position.y += manual_position_offset[1]
+    updated_map.info.origin.position.z += manual_position_offset[2]
+    manual_offset_quaternion = tf.transformations.quaternion_from_euler(manual_orientation_offset[0], manual_orientation_offset[1], manual_orientation_offset[2])
+    quaternion_combined = tf.transformations.quaternion_multiply(updated_map.info.origin.orientation, manual_offset_quaternion)
+    updated_map.info.origin.orientation.x = quaternion_combined[0]
+    updated_map.info.origin.orientation.y = quaternion_combined[1]
+    updated_map.info.origin.orientation.z = quaternion_combined[2]
+    updated_map.info.origin.orientation.w = quaternion_combined[3]
+
+    # Add human transform
+    if is_localized:
+        # Add human to robot world transform (all applied) 
+        updated_map.info.origin.position.x += human_localization_pose.pose.pose.position.x
+        updated_map.info.origin.position.y += human_localization_pose.pose.pose.position.y
+        updated_map.info.origin.position.z += human_localization_pose.pose.pose.position.z
+        quaternion_combined = tf.transformations.quaternion_multiply(updated_map.info.origin.orientation, human_localization_pose.pose.pose.orientation)
+        updated_map.info.origin.orientation.x = quaternion_combined[0]
+        updated_map.info.origin.orientation.y = quaternion_combined[1]
+        updated_map.info.origin.orientation.z = quaternion_combined[2]
+        updated_map.info.origin.orientation.w = quaternion_combined[3]
+    else:
+        # Add human height (only z height) 
+        updated_map.info.origin.position.z += z_offset_human 
+
+    update_map_origin(tf_transform)
+
+    map_pub.publish(updated_map)
+
+def human_localization_callback(msg):
+    global human_localization_pose
+    human_localization_pose = msg
+
+def is_localized_callback(msg):
+    global is_localized
+    is_localized = msg.data
+    if is_localized and (human_localization_pose.pose.pose.position.x != 0 or 
+                     human_localization_pose.pose.pose.position.y != 0 or 
+                     human_localization_pose.pose.pose.position.z != 0):
+        rospy.Timer(rospy.Duration(1), stop_human_localization_subscriber, oneshot=True)
+
+def stop_human_localization_subscriber(event):
+    global human_localization_subscriber
+    human_localization_subscriber.unregister()
+
 if __name__ == "__main__":
     rospy.init_node('occupancygrid_transformer')
     parent_frame = rospy.get_param("~parent_frame", "map")
     child_frame = rospy.get_param("~child_frame","robot/map")
-    origin_position_offset = rospy.get_param('~origin_position_offset', [0.0, 0.0, 0.0])
-    origin_orientation_offset = rospy.get_param('~origin_orientation_offset', [0.0, 0.0, 0.0])
+    z_offset_human = rospy.get_param("~z_offset_human", 0)
+    z_offset_floor = rospy.get_param("~z_offset_floor", 0)
+    manual_position_offset = rospy.get_param('~manual_position_offset', [0.0, 0.0, 0.0])
+    manual_orientation_offset = rospy.get_param('~manual_orientation_offset', [0.0, 0.0, 0.0])
 
-    floor_transform = Transform()
+    tf_transform = Transform()
     tfBuffer = tf2_ros.Buffer()
     listener = tf2_ros.TransformListener(tfBuffer)
 
     rate = rospy.Rate(10.0)
     map_pub = rospy.Publisher("/global/map", OccupancyGrid, queue_size=50)
+    
 
-    original_map = OccupancyGrid()
-    updated_map = OccupancyGrid()
+    original_map = OccupancyGrid()      
     rospy.Subscriber("/robot/map", OccupancyGrid, map_callback)
 
+    is_localized = False
+    human_localization_pose = Odometry()
+    rospy.Subscriber("/is_localized", Bool, is_localized_callback)
+    human_localization_subscriber = rospy.Subscriber("/human_localization_odom", Odometry, human_localization_callback)
+
+
     while not rospy.is_shutdown():
+        updated_map = OccupancyGrid()    
         try:
-            floor_transform = tfBuffer.lookup_transform(parent_frame,child_frame,rospy.Time())
-        except tf2.LookupException as e:
-            rospy.logwarn("Robot Transform lookup for Floor offset failed: {}".format(e))
+            tf_transform = tfBuffer.lookup_transform(parent_frame,child_frame,rospy.Time())
+            updated_map.header.frame_id = tf_transform.header.frame_id
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
             rate.sleep()
             continue
+
         
-        floor_transform.transform.translation.x += origin_position_offset[0]
-        floor_transform.transform.translation.y += origin_position_offset[1]
-        floor_transform.transform.translation.z += origin_position_offset[2]
-        quaternion = quaternion_from_euler(origin_orientation_offset[0], origin_orientation_offset[1], origin_orientation_offset[2])
-        floor_transform.transform.rotation.x += quaternion[0]
-        floor_transform.transform.rotation.y += quaternion[1]
-        floor_transform.transform.rotation.z += quaternion[2]
-        floor_transform.transform.rotation.w += quaternion[3]
-
-        update_map_origin(floor_transform)
-
-        map_pub.publish(updated_map)
 
         rate.sleep()
